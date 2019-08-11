66a9b9ca3b7799e28f24f75aaba0a8a5596df3a6
MarcosVinicius1994 <marcosvtn@yahoo.com.br>
2019-08-11 11:41:56 -0300

    Atualização
    commit3#

--- BackEnd/src/routes.js
+++ BackEnd/src/routes.js
@@ -1,17 +1,10 @@
 const express = require('express');
+const DevController = require('./controllers/DevController');
 
 const routes = express.Router();
 
-routes.get('/',(req, res)=>{
 
-    // return  res.send(`Olá ${req.query.name}`);
-    return  res.json({message:`Olá  ${req.query.name}`});
-});
 
-routes.post('/devs',(req, res)=>{
-
-    console.log(req.body);
-    return  res.json({ok: true});
-});
+routes.post('/devs', DevController.store);
 
 module.exports = routes;    
--- BackEnd/src/server.js
+++ BackEnd/src/server.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const routes = require('./routes');
+
 const server = express();
 
+mongoose.connect('mongodb+srv://omnistackMVTN:12345678mvtn@cluster0-eeung.mongodb.net/omnistack8?retryWrites=true&w=majority', {useNewUrlParser: true} );
 server.use(express.json());
 server.use(routes);
 server.listen(3333);
